099b831afc7d179995141ba19c3c3ca1e9eb252d
Anastasia Raeva <anastassia.raeva@gmail.com>
2021-03-29 14:10:21 +0300

    Add animation for changing images

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const lampDesc = document.querySelector('.main__lamp-desc');
-const form = document.querySelector('.main__form-set--top');
+const formTopRadio = document.querySelector('.main__form-set--top');
 const lampImgBig = document.querySelector('.main__lamp-img');
 const changedImg = document.querySelector('.main__changed-img');
+const changedImgWrapper = document.querySelector('.main__changed-lamp');
+const lampImgBigWrap = document.querySelector('.main__lamp');
+const radioLamps = document.querySelectorAll('.main__form-label');
+const formBottomRadio = document.querySelector('.main__form-set--bottom');
+const imgRoom = document.querySelector('.main__wrapper-img');
+const radioLight = document.querySelectorAll('.main__form-lab');
 
 const firebaseConfig = {
     apiKey: "AIzaSyBmstXF59RerQnKuBaoACgK70SHogA1mDg",
@@ -55,20 +61,44 @@ function addDescription(lamp) {
 
 function changeBigImg(id) {
     lampImgBig.src = `img/${id}.png`;
+    lampImgBigWrap.classList.add('animation__to-right');
 }
 
 function changeSmallImg(id) {
     changedImg.src = `img/${id}.png`;
+    changedImgWrapper.classList.add('animation__to-right');
 }
 
+function changeLight(target) {
+    if (target === 'day') {
+        imgRoom.classList.remove('main__wrapper-img--night');
+    } else {
+        imgRoom.classList.add('main__wrapper-img--night');
+    }
+}
 
-async function handleClick(event) {
+async function handleClickTop(event) {
     event.preventDefault();
+    radioLamps.forEach((item) => item.classList.remove('main__form-label--checked'));
+    changedImgWrapper.classList.remove('animation__to-right');
+    lampImgBigWrap.classList.remove('animation__to-right');
+    lampDesc.classList.remove('animation__to-center');
+    event.target.classList.add('main__form-label--checked');
     let id = event.target.getAttribute('for');
     let lamp = await getLampData(id);
     addDescription(lamp);
     changeBigImg(id);
     changeSmallImg(id);
+    lampDesc.classList.add('animation__to-center');
 }
 
-form.addEventListener('click', handleClick);
+function handleClickBottom(event) {
+    event.preventDefault();
+    radioLight.forEach((item) => item.classList.remove('main__form-lab--checked'));
+    let target = event.target.getAttribute('for');
+    changeLight(target);
+    event.target.classList.add('main__form-lab--checked');
+}
+
+formTopRadio.addEventListener('click', handleClickTop);
+formBottomRadio.addEventListener('click', handleClickBottom);
